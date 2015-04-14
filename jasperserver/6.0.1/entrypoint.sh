db749722103b69526158be09f01931c3b2614292
derekha2010 <derekha2005@gmail.com>
2015-04-14 15:18:29 +0800

    Update entrypoint.sh

--- jasperserver/6.0.1/entrypoint.sh
+++ jasperserver/6.0.1/entrypoint.sh
@@ -2,14 +2,13 @@
 set -e
 
 setup_jasperserver() {
-    DB_TYPE=${DB_TYPE:-postgresql}
-    DB_HOST=${DB_HOST:-localhost}
-    DB_USER=${DB_USER:-postgres}
-    DB_PASSWORD=${DB_PASSWORD:-postgres}
+    DB_TYPE=${DB_TYPE:-mysql}
+    DB_USER=${DB_USER:-root}
+    DB_PASSWORD=${DB_PASSWORD:-mysql}
 
     pushd /usr/src/jasperreports-server/buildomatic
     cp sample_conf/${DB_TYPE}_master.properties default_master.properties
-    sed -i -e "s|^appServerDir.*$|appServerDir = $CATALINA_HOME|g; s|^dbHost.*$|dbHost=$DB_HOST|g; s|^dbUsername.*$|dbUsername=$DB_USER|g; s|^dbPassword.*$|dbPassword=$DB_PASSWORD|g" default_master.properties
+    sed -i -e "s|^appServerDir.*$|appServerDir = $CATALINA_HOME|g; s|^dbHost.*$|dbHost=$MYSQL_PORT_3306_TCP_ADDR|g; s|^dbUsername.*$|dbUsername=$DB_USER|g; s|^dbPassword.*$|dbPassword=$DB_PASSWORD|g" default_master.properties
 
     for i in $@; do
         ./js-ant $i
@@ -30,6 +29,14 @@ seed_database() {
     setup_jasperserver create-js-db init-js-db-ce import-minimal-ce
 }
 
+seed_and_run() {
+    if [ ! -d "$CATALINA_HOME/webapps/jasperserver" ]; then
+        setup_jasperserver create-js-db init-js-db-ce import-minimal-ce deploy-webapp-ce
+    fi
+
+    catalina.sh run
+}
+
 case "$1" in
     run)
         shift 1
@@ -38,6 +45,9 @@ case "$1" in
     seed)
         seed_database
         ;;
+    seedAndRun)
+        seed_and_run
+        ;;        
     *)
         exec "$@"
 esac
